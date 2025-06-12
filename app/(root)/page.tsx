0da8095f00707bd772fe9c38e213829a94282dd4
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-06-12 15:39:42 +0100

    aos removed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,16 +7,16 @@ import HowItWorks from "@/components/home/HowItWorks";
 import LessionSection from "@/components/home/LessionSection";
 import Newsletter from "@/components/home/Newsletter";
 import TestimonialSection from "@/components/home/Testmony";
-import AOS from "aos";
+// import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration in milliseconds
-      once: false, // Whether animation should happen only once
-    });
+    // AOS.init({
+    //   duration: 1000, // Animation duration in milliseconds
+    //   once: false, // Whether animation should happen only once
+    // });
   }, []);
   return (
     <div className="min-h-screen flex flex-col">
